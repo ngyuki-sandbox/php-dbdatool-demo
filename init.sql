73ef3e04ed268a70081b39188a47150403104afb
ngyuki <ngyuki.jp@gmail.com>
2018-07-16 08:21:00 +0900

    fix init.sql

--- init.sql
+++ init.sql
@@ -9,10 +9,14 @@ CREATE TABLE t_group (
 );
 
 CREATE TABLE t_user (
+    user_id INT NOT NULL AUTO_INCREMENT,
     group_id INT NOT NULL,
-    user_id INT NOT NULL,
     username VARCHAR(255),
-    email VARCHAR(100),
+    email VARCHAR(255),
+    remarks TEXT,
 
-    PRIMARY KEY (group_id, user_id)
+    PRIMARY KEY (user_id)
 );
+
+ALTER TABLE t_user ADD CONSTRAINT fk_user__group FOREIGN KEY (group_id) REFERENCES t_group (group_id) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE t_user ADD UNIQUE INDEX u_email (email);
